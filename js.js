44563655367d84141b5218ce417dbeaded1f9874
zykovanatoliy9415-hub <zykov.anatoliy9415@gmail.com>
2025-09-07 22:22:12 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+var button = document.querySelector("button");
+button.addEventListener ("click", function() {
+    console.log("log: Выведи этот текст в консоль если кнопка нажмётся");
+    alert("Никогда не сдавайся");
+})
